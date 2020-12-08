9f535b7e8e6892ba879bfda906089aab85a9695c
Sebastian Serritella <sserritella@sneakersagency.com>
2020-12-08 15:04:55 -0300

    Single instance of viewmodel

--- app/src/main/java/com/reign/test/di/ViewModelModule.kt
+++ app/src/main/java/com/reign/test/di/ViewModelModule.kt
@@ -6,6 +6,6 @@ import org.koin.android.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
 val viewModelModule = module {
-    viewModel { ArticlesViewModel(get()) }
+    single { ArticlesViewModel(get()) }
     viewModel { HitViewModel() }
 }
